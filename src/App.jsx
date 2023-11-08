98c99a77eaea5fa70564dadd8f0631f5a13f20d9
Dominik Hämmerle <dominikhammerle@MacBook-Pro.local>
2023-11-08 14:13:24 +0100

    CSS Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Form from './components/Form';
 
 function App() {
   return (
-    <>
+    <div className='website'>
     <h1>Osteria Corte dei Frati - Zürich</h1>
     <Form/>
-    </>
+    </div>
   );
 }
 
